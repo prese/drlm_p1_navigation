789be095fc6fde5ae1d9635d4114f97785019dcb
prese <sebastian.presecan@telios.ro>
2019-03-11 07:08:18 +0200

    adding dueling QN

--- model.py
+++ model.py
@@ -25,4 +25,41 @@ class QNetwork(nn.Module):
         """Build a network that maps state -> action values."""
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
-        return self.fc3(x)
+        return self.fc3(x)          
+
+class DuelingDQN(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+        """
+        super(DuelingDQN, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, 64)
+
+        self.fc1_value = nn.Linear(64, 64)
+        self.fc2_value = nn.Linear(64, 64)
+        self.out_value = nn.Linear(64, 1)
+
+        self.fc1_action = nn.Linear(64, 64)
+        self.fc2_action = nn.Linear(64, 64)
+        self.out_action = nn.Linear(64, action_size)
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.relu(self.fc1(state))
+
+        x_value = F.relu(self.fc1_value(x))
+        x_value = F.relu(self.fc2_value(x_value))
+        x_value = self.out_value(x_value)
+
+        x_action = F.relu(self.fc1_action(x))
+        x_action = F.relu(self.fc2_action(x_action))
+        x_action = self.out_action(x_action)
+
+        return x_value + x_action - x_action.mean()
